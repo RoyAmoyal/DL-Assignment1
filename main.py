56a4ffb2dd5ff4539c2f7f66d8e880436863c4fb
koren <koren121222@gmail.com>
2022-05-24 20:46:55 +0300

    finito la comedia?

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import layers
 import optimizers
 import models
 import pandas as pd
-from network_tests import grad_test_w, grad_test_b, jacobian_test_w, jacobian_test_b, grad_test_whole_network_w, grad_test_whole_network_b
+from network_tests import grad_test_w, grad_test_b, jacobian_test_w1_resnet, jacobian_test_w2_resnet, jacobian_test_b, jacobian_test_w, grad_test_whole_network_w, grad_test_whole_network_b, jacobian_test_b1_resnet, grad_test_whole_resnet_network_w, grad_test_whole_resnet_network_b,  jacobian_test_b2_resnet
 from LinearRegression import loss_func_SGD
 from matplotlib import pyplot as plt
 import time
@@ -17,19 +17,19 @@ def read_mat(name):
 
 if __name__=="__main__":
     # SGD test on least squares
-    # theta = np.random.rand(2)  # m, b: mx+b
-    # data = np.random.rand(2, 10)
-    # x = data[0, :]
-    # y = data[1,:]
-    # for i in range(100000):
-    #     theta = loss_func_SGD(data,theta,learning_rate=0.1)
-    # m,b = theta
-    # plt.scatter(x, y)
-    # plt.plot([min(x), max(x)], [min(m * x + b), max(m * x + b)], color='green')  # regression line
-    # plt.show()
+    theta = np.random.rand(2)  # m, b: mx+b
+    data = np.random.rand(2, 10)
+    x = data[0, :]
+    y = data[1,:]
+    for i in range(100000):
+        theta = loss_func_SGD(data,theta,learning_rate=0.1)
+    m,b = theta
+    plt.scatter(x, y)
+    plt.plot([min(x), max(x)], [min(m * x + b), max(m * x + b)], color='green')  # regression line
+    plt.show()
 
     # HyperParams
-    batch_size = 100
+    batch_size = 20
     num_epochs = 300
     num_classes = 2
     hidden_units = 100
@@ -41,8 +41,10 @@ if __name__=="__main__":
     # PeaksData  da, SwissRollData, GMMData
     X_train, y_train, X_test, y_test = utils.get_data('SwissRollData')
     X_train, y_train = shuffle(X_train, y_train)
-    #X_train= X_train[:200,:]
-    #y_train= y_train[:200]
+
+    # 2.2.5
+    # X_train= X_train[:200,:]
+    # y_train= y_train[:200]
 
     # test of softmax (2.1.3))
     softmax_model = models.MyNeuralNetwork()
@@ -51,18 +53,27 @@ if __name__=="__main__":
     losses, train_accuracy, test_accuracy = softmax_model.fit(X_train, y_train, X_test, y_test, batch_size, num_epochs,
                                                       optimizer)
     utils.plot_scores(train_accuracy, test_accuracy)
-    exit()
+
 
     # gradient and jacobian tests
     grad_test_w(X_train, y_train)  # for the softmax
     grad_test_b(X_train, y_train)  # for the softmax
-    jacobian_test_w(X_train, y_train, network='Linear')
-    jacobian_test_b(X_train, y_train, network='Linear')
-    grad_test_whole_network_w(X_train, y_train, network='Linear')
-    grad_test_whole_network_b(X_train, y_train, network='Linear')
 
-    exit()
+    jacobian_test_w(X_train, y_train)  # jacobian tests
+    jacobian_test_b(X_train, y_train)
 
+    jacobian_test_w1_resnet(X_train, y_train)  # jacobian test sfo resnet
+    jacobian_test_w2_resnet(X_train, y_train)
+    jacobian_test_b1_resnet(X_train, y_train)
+    jacobian_test_b2_resnet(X_train, y_train)
+
+    grad_test_whole_network_w(X_train, y_train, layer_number=1)  # testing the whole networks
+    grad_test_whole_network_b(X_train, y_train, layer_number=1)
+    grad_test_whole_resnet_network_w(X_train, y_train, layer_number=1)
+    grad_test_whole_resnet_network_b(X_train, y_train, layer_number=1)
+
+
+    # running the model
     model = models.MyNeuralNetwork()
     if network == 'ResNet':
         model.add(layers.ResBlock(dimensions, hidden_units))
--- network_tests.py
+++ network_tests.py
@@ -118,15 +118,12 @@ def grad_test_b(X_train, y_train):
     plt.show()
 
 
-def jacobian_test_w(X_train, y_train, network="Linear"):
+def jacobian_test_w(X_train, y_train):
     softmax_in = 2
     softmax_out = 5
     hidden_units = 10
     model = models.MyNeuralNetwork()
-    if network == 'Linear':
-        model.add(layers.Linear(softmax_in, hidden_units))
-    else:  # resnet
-        model.add(layers.ResBlock(softmax_in, hidden_units))
+    model.add(layers.Linear(softmax_in, hidden_units))
     model.add(activations.Tanh())
     model.add(layers.Softmax(hidden_units, softmax_out))
     model.init()
@@ -184,15 +181,138 @@ def jacobian_test_w(X_train, y_train, network="Linear"):
     plt.show()
 
 
-def jacobian_test_b(X_train, y_train, network="Linear"):
+def jacobian_test_w1_resnet(X_train, y_train):
     softmax_in = 2
     softmax_out = 5
     hidden_units = 10
     model = models.MyNeuralNetwork()
-    if network == 'Linear':
-        model.add(layers.Linear(softmax_in, hidden_units))
-    else:  # resnet
-        model.add(layers.ResBlock(softmax_in, hidden_units))
+    model.add(layers.ResBlock(softmax_in, hidden_units))
+    model.add(activations.Tanh())
+    model.add(layers.Softmax(hidden_units, softmax_out))
+    model.init()
+    for p in model.parameters:
+        p.grad = 0.
+
+    eps0 = 1
+    eps = np.array([(0.5 ** i) * eps0 for i in range(8)])
+
+    d = np.random.random((2, 10))
+    d = d / np.sum(d)
+
+    x_data = np.array([X_train[0]])
+    x_label = np.array([y_train[0]])
+
+    grad_diff = []
+
+    for epss in eps:
+        model_grad = copy.deepcopy(model)
+        probabilities_grad = model_grad.forward(x_data)
+        model2 = copy.deepcopy(model)
+        model2.graph[0].weights1.data += d*epss
+        probabilities_grad2 = model2.forward(x_data)
+
+        f_x_eps_d = model2.graph[1].activation_output
+        f_x = model_grad.graph[1].activation_output
+
+        grad_diff.append(LA.norm(f_x_eps_d - f_x))
+    print(grad_diff)
+    plt.figure(figsize=(12, 8))
+    x = [1, 2, 3, 4, 5, 6, 7, 8]
+    plt.plot(x, grad_diff, label='zero order')
+
+    new_grad = []
+    for epss in eps:
+        model_grad = copy.deepcopy(model)
+        probabilities_grad = copy.deepcopy(model_grad.forward(x_data))
+        model2 = copy.deepcopy(model)
+        model2.graph[0].weights1.data += d * epss
+        probabilities_grad2 = copy.deepcopy(model2.forward(x_data))
+        model_grad.backward(x_label)
+        f_x_eps_d = model2.graph[1].activation_output
+        f_x = model_grad.graph[1].activation_output
+        grad = model_grad.graph[0].weights1.grad
+        JacMV = epss * np.matmul(d.T, grad)
+        diff = LA.norm(f_x_eps_d - f_x - JacMV)
+        new_grad.append(diff*epss)
+
+    plt.plot(x, new_grad, label='first order')
+    plt.legend()
+    plt.title('Jacobian test ResNet for w1')
+    plt.ylabel('error')
+    plt.xlabel('k')
+    plt.semilogy([1, 2, 3, 4, 5, 6, 7, 8], grad_diff)
+    plt.show()
+
+
+def jacobian_test_w2_resnet(X_train, y_train):
+    softmax_in = 2
+    softmax_out = 5
+    hidden_units = 10
+    model = models.MyNeuralNetwork()
+    model.add(layers.ResBlock(softmax_in, hidden_units))
+    model.add(activations.Tanh())
+    model.add(layers.Softmax(hidden_units, softmax_out))
+    model.init()
+    for p in model.parameters:
+        p.grad = 0.
+
+    eps0 = 1
+    eps = np.array([(0.5 ** i) * eps0 for i in range(8)])
+
+    d = np.random.random((2, 10))
+    d = d / np.sum(d)
+
+    x_data = np.array([X_train[0]])
+    x_label = np.array([y_train[0]])
+
+    grad_diff = []
+
+    for epss in eps:
+        model_grad = copy.deepcopy(model)
+        probabilities_grad = model_grad.forward(x_data)
+        model2 = copy.deepcopy(model)
+        model2.graph[0].weights2.data += d*epss
+        probabilities_grad2 = model2.forward(x_data)
+
+        f_x_eps_d = model2.graph[1].activation_output
+        f_x = model_grad.graph[1].activation_output
+
+        grad_diff.append(LA.norm(f_x_eps_d - f_x))
+    print(grad_diff)
+    plt.figure(figsize=(12, 8))
+    x = [1, 2, 3, 4, 5, 6, 7, 8]
+    plt.plot(x, grad_diff, label='zero order')
+
+    new_grad = []
+    for epss in eps:
+        model_grad = copy.deepcopy(model)
+        probabilities_grad = copy.deepcopy(model_grad.forward(x_data))
+        model2 = copy.deepcopy(model)
+        model2.graph[0].weights2.data += d * epss
+        probabilities_grad2 = copy.deepcopy(model2.forward(x_data))
+        model_grad.backward(x_label)
+        f_x_eps_d = model2.graph[1].activation_output
+        f_x = model_grad.graph[1].activation_output
+        grad = model_grad.graph[0].weights2.grad
+        JacMV = epss * np.matmul(d.T, grad)
+        diff = LA.norm(f_x_eps_d - f_x - JacMV)
+        new_grad.append(diff*epss)
+
+    plt.plot(x, new_grad, label='first order')
+    plt.legend()
+    plt.title('Jacobian test ResNet for w2')
+    plt.ylabel('error')
+    plt.xlabel('k')
+    plt.semilogy([1, 2, 3, 4, 5, 6, 7, 8], grad_diff)
+    plt.show()
+
+
+def jacobian_test_b(X_train, y_train):
+    softmax_in = 2
+    softmax_out = 5
+    hidden_units = 10
+    model = models.MyNeuralNetwork()
+    model.add(layers.Linear(softmax_in, hidden_units))
     model.add(activations.Tanh())
     model.add(layers.Softmax(hidden_units, softmax_out))
     model.init()
@@ -250,16 +370,142 @@ def jacobian_test_b(X_train, y_train, network="Linear"):
     plt.show()
 
 
-def grad_test_whole_network_w(X_train, y_train, network='Linear'):
+def jacobian_test_b1_resnet(X_train, y_train):
+    softmax_in = 2
+    softmax_out = 5
+    hidden_units = 10
+    model = models.MyNeuralNetwork()
+    model.add(layers.ResBlock(softmax_in, hidden_units))
+    model.add(activations.Tanh())
+    model.add(layers.Softmax(hidden_units, softmax_out))
+    model.init()
+    for p in model.parameters:
+        p.grad = 0.
+
+    eps0 = 1
+    eps = np.array([(0.5 ** i) * eps0 for i in range(8)])
+
+    d = np.random.random((1, 10))
+    d = d / np.sum(d)
+
+    x_data = np.array([X_train[0]])
+    x_label = np.array([y_train[0]])
+
+    grad_diff = []
+
+    for epss in eps:
+        model_grad = copy.deepcopy(model)
+        probabilities_grad = model_grad.forward(x_data)
+        model2 = copy.deepcopy(model)
+        model2.graph[0].bias1.data += d*epss
+        probabilities_grad2 = model2.forward(x_data)
+
+        f_x_eps_d = model2.graph[1].activation_output
+        f_x = model_grad.graph[1].activation_output
+
+        grad_diff.append(LA.norm(f_x_eps_d - f_x))
+    print(grad_diff)
+    plt.figure(figsize=(12, 8))
+    x = [1, 2, 3, 4, 5, 6, 7, 8]
+    plt.plot(x, grad_diff, label='zero order')
+
+    new_grad = []
+    for epss in eps:
+        model_grad = copy.deepcopy(model)
+        probabilities_grad = copy.deepcopy(model_grad.forward(x_data))
+        model2 = copy.deepcopy(model)
+        model2.graph[0].bias1.data += d * epss
+        probabilities_grad2 = copy.deepcopy(model2.forward(x_data))
+        model_grad.backward(x_label)
+        f_x_eps_d = model2.graph[1].activation_output
+        f_x = model_grad.graph[1].activation_output
+        grad = model_grad.graph[0].bias1.grad
+        JacMV = epss * np.matmul(d.T, grad)
+        diff = LA.norm(f_x_eps_d - f_x - JacMV)
+        new_grad.append(diff*epss)
+
+    plt.plot(x, new_grad, label='first order')
+    plt.legend()
+    plt.title('Jacobian test ResNet for bias1')
+    plt.ylabel('error')
+    plt.xlabel('k')
+    plt.semilogy([1, 2, 3, 4, 5, 6, 7, 8], grad_diff)
+    plt.show()
+
+
+def jacobian_test_b2_resnet(X_train, y_train):
+    softmax_in = 2
+    softmax_out = 5
+    hidden_units = 10
+    model = models.MyNeuralNetwork()
+    model.add(layers.ResBlock(softmax_in, hidden_units))
+    model.add(activations.Tanh())
+    model.add(layers.Softmax(hidden_units, softmax_out))
+    model.init()
+    for p in model.parameters:
+        p.grad = 0.
+
+    eps0 = 1
+    eps = np.array([(0.5 ** i) * eps0 for i in range(8)])
+
+    d = np.random.random((1, 10))
+    d = d / np.sum(d)
+
+    x_data = np.array([X_train[0]])
+    x_label = np.array([y_train[0]])
+
+    grad_diff = []
+
+    for epss in eps:
+        model_grad = copy.deepcopy(model)
+        probabilities_grad = model_grad.forward(x_data)
+        model2 = copy.deepcopy(model)
+        model2.graph[0].bias2.data += d*epss
+        probabilities_grad2 = model2.forward(x_data)
+
+        f_x_eps_d = model2.graph[1].activation_output
+        f_x = model_grad.graph[1].activation_output
+
+        grad_diff.append(LA.norm(f_x_eps_d - f_x))
+    print(grad_diff)
+    plt.figure(figsize=(12, 8))
+    x = [1, 2, 3, 4, 5, 6, 7, 8]
+    plt.plot(x, grad_diff, label='zero order')
+
+    new_grad = []
+    for epss in eps:
+        model_grad = copy.deepcopy(model)
+        probabilities_grad = copy.deepcopy(model_grad.forward(x_data))
+        model2 = copy.deepcopy(model)
+        model2.graph[0].bias2.data += d * epss
+        probabilities_grad2 = copy.deepcopy(model2.forward(x_data))
+        model_grad.backward(x_label)
+        f_x_eps_d = model2.graph[1].activation_output
+        f_x = model_grad.graph[1].activation_output
+        grad = model_grad.graph[0].bias2.grad
+        JacMV = epss * np.matmul(d.T, grad)
+        diff = LA.norm(f_x_eps_d - f_x - JacMV)
+        new_grad.append(diff*epss)
+
+    plt.plot(x, new_grad, label='first order')
+    plt.legend()
+    plt.title('Jacobian test ResNet for bias2')
+    plt.ylabel('error')
+    plt.xlabel('k')
+    plt.semilogy([1, 2, 3, 4, 5, 6, 7, 8], grad_diff)
+    plt.show()
+
+
+def grad_test_whole_network_w(X_train, y_train, layer_number=1):
     softmax_in = 2
     softmax_out = 5
     hidden_units = 5
     model = models.MyNeuralNetwork()
-    if network == 'Linear': # resnet
-        model.add(layers.Linear(softmax_in, hidden_units))
-    else:
-        model.add(layers.ResBlock(softmax_in, hidden_units))
+    model.add(layers.Linear(softmax_in, hidden_units))
     model.add(activations.Tanh())
+    for i in range(layer_number-1):
+        model.add(layers.Linear(hidden_units, hidden_units))
+        model.add(activations.Tanh())
     model.add(layers.Softmax(hidden_units, softmax_out))
     model.init()
     for p in model.parameters:
@@ -310,16 +556,16 @@ def grad_test_whole_network_w(X_train, y_train, network='Linear'):
     plt.show()
 
 
-def grad_test_whole_network_b(X_train, y_train, network='Linear'):
+def grad_test_whole_network_b(X_train, y_train, layer_number=1):
     softmax_in = 2
     softmax_out = 5
     hidden_units = 5
     model = models.MyNeuralNetwork()
-    if network == 'Linear': # resnet
-        model.add(layers.Linear(softmax_in, hidden_units))
-    else:
-        model.add(layers.ResBlock(softmax_in, hidden_units))
+    model.add(layers.Linear(softmax_in, hidden_units))
     model.add(activations.Tanh())
+    for i in range(layer_number-1):
+        model.add(layers.Linear(hidden_units, hidden_units))
+        model.add(activations.Tanh())
     model.add(layers.Softmax(hidden_units, softmax_out))
     model.init()
     for p in model.parameters:
@@ -370,5 +616,121 @@ def grad_test_whole_network_b(X_train, y_train, network='Linear'):
     plt.show()
 
 
+def grad_test_whole_resnet_network_w(X_train, y_train, layer_number=1):
+    softmax_in = 2
+    softmax_out = 5
+    hidden_units = 5
+    model = models.MyNeuralNetwork()
+    model.add(layers.ResBlock(softmax_in, hidden_units))
+    model.add(activations.Tanh())
+    for i in range(layer_number-1):
+        model.add(layers.ResBlock(hidden_units, hidden_units))
+        model.add(activations.Tanh())
+    model.add(layers.Softmax(hidden_units, softmax_out))
+    model.init()
+    for p in model.parameters:
+        p.grad = 0.
+
+    eps0 = 1
+    eps = np.array([(0.5 ** i) * eps0 for i in range(8)])
+
+    d = np.random.random((5, 5))
+    d = d / np.sum(d)
+    grad_diff = []
+
+    x_data = np.array([X_train[0]])
+    x_label = np.array([y_train[0]])
+
+    for epss in eps:
+        model_grad = copy.deepcopy(model)
+        probabilities_grad = model_grad.forward(x_data)
+        model2 = copy.deepcopy(model)
+        model2.graph[2].weights.data += d * epss
+        probabilities_grad2 = model2.forward(x_data)
+        grad_diff.append(np.abs(utils.cross_entropy_loss(probabilities_grad2, x_label) -
+                                utils.cross_entropy_loss(probabilities_grad, x_label)))
+
+    print(grad_diff)
+    plt.figure(figsize=(12, 8))
+    x = [1, 2, 3, 4, 5, 6, 7, 8]
+    plt.plot(x, grad_diff, label='zero order')
+    new_grad = []
+    for epss in eps:
+        model_grad = copy.deepcopy(model)
+        probabilities_grad = copy.deepcopy(model_grad.forward(x_data))
+        model2 = copy.deepcopy(model)
+        model2.graph[2].weights.data += d * epss
+        probabilities_grad2 = copy.deepcopy(model2.forward(x_data))
+        model2.backward(x_label)
+        grad_x = model2.graph[2].weights.grad
+        new_grad.append(np.abs(utils.cross_entropy_loss(probabilities_grad2, x_label) -
+                                utils.cross_entropy_loss(probabilities_grad, x_label) -
+                                epss * np.dot(d.flatten().T, grad_x.flatten())))
+
+    plt.plot(x, new_grad, label='first order')
+    plt.legend()
+    plt.title('Gradient test for the whole resnet network (weights)')
+    plt.ylabel('error')
+    plt.xlabel('k')
+    plt.semilogy([1, 2, 3, 4, 5, 6, 7, 8], grad_diff)
+    plt.show()
 
 
+def grad_test_whole_resnet_network_b(X_train, y_train, layer_number=1):
+    softmax_in = 2
+    softmax_out = 5
+    hidden_units = 5
+    model = models.MyNeuralNetwork()
+    model.add(layers.ResBlock(softmax_in, hidden_units))
+    model.add(activations.Tanh())
+    for i in range(layer_number-1):
+        model.add(layers.ResBlock(hidden_units, hidden_units))
+        model.add(activations.Tanh())
+    model.add(layers.Softmax(hidden_units, softmax_out))
+    model.init()
+    for p in model.parameters:
+        p.grad = 0.
+
+    eps0 = 1
+    eps = np.array([(0.5 ** i) * eps0 for i in range(8)])
+
+    d = np.random.random((1, 5))
+    d = d / np.sum(d)
+    grad_diff = []
+
+    x_data = np.array([X_train[0]])
+    x_label = np.array([y_train[0]])
+
+    for epss in eps:
+        model_grad = copy.deepcopy(model)
+        probabilities_grad = model_grad.forward(x_data)
+        model2 = copy.deepcopy(model)
+        model2.graph[2].bias.data += d * epss
+        probabilities_grad2 = model2.forward(x_data)
+        grad_diff.append(np.abs(utils.cross_entropy_loss(probabilities_grad2, x_label) -
+                                utils.cross_entropy_loss(probabilities_grad, x_label)))
+
+    print(grad_diff)
+    plt.figure(figsize=(12, 8))
+    x = [1, 2, 3, 4, 5, 6, 7, 8]
+    plt.plot(x, grad_diff, label='zero order')
+    new_grad = []
+    for epss in eps:
+        model_grad = copy.deepcopy(model)
+        probabilities_grad = copy.deepcopy(model_grad.forward(x_data))
+        model2 = copy.deepcopy(model)
+        model2.graph[2].bias.data += d * epss
+        probabilities_grad2 = copy.deepcopy(model2.forward(x_data))
+        model2.backward(x_label)
+        grad_x = model2.graph[2].bias.grad
+        new_grad.append(np.abs(utils.cross_entropy_loss(probabilities_grad2, x_label) -
+                                utils.cross_entropy_loss(probabilities_grad, x_label) -
+                                epss * np.dot(d.flatten().T, grad_x.flatten())))
+
+    plt.plot(x, new_grad, label='first order')
+    plt.legend()
+    plt.title('Gradient test for the whole network (biases)')
+    plt.ylabel('error')
+    plt.xlabel('k')
+    plt.semilogy([1, 2, 3, 4, 5, 6, 7, 8], grad_diff)
+    plt.show()
